7d3c245ae706bccd2b85f331b8d58bc4ad4c93e0
Tymon Abramczyk <tymon.abramczyk@gmail.com>
2026-06-13 22:04:02 +0200

    chore: create Retrofit instance

---
+++ app/src/main/java/com/example/blogapp/data/remote/RetrofitInstance.kt
@@ -0,0 +1,33 @@
+package com.example.blogapp.data.remote
+
+import com.example.blogapp.BuildConfig
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+object RetrofitInstance {
+
+    private const val BASE_URL = BuildConfig.BASE_URL
+
+    private val client = OkHttpClient.Builder()
+        .addInterceptor(HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        })
+        .connectTimeout(30, TimeUnit.SECONDS)
+        .readTimeout(30, TimeUnit.SECONDS)
+        .build()
+
+    private val retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(client)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    val api: ApiService by lazy {
+        retrofit.create(ApiService::class.java)
+    }
+}
